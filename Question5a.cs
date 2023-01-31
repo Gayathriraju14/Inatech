6a3b7f55990e5cfb229d232cc544e10d2f057b1a
Gayathri146 <103047218+Gayathriraju14@users.noreply.github.com>
2023-01-31 13:39:04 +0530

    Add files via upload

---
+++ Question5a.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Question5a
+    {
+        string Name;
+        int Age;
+        public Question5a()
+        {
+            Name = "Gayathri";
+            Age = 21;
+        }
+        public Question5a(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+        public static void Main(string[] args)
+        {
+            Question5a dobj = new Question5a();
+            Console.WriteLine(dobj.Name);
+            Console.WriteLine(dobj.Age);
+            Question5a bobj = new Question5a("Gayu", 22);
+            Console.WriteLine(bobj.Name);
+            Console.WriteLine(bobj.Age);
+        }
+    }
+}
+
+
+
+

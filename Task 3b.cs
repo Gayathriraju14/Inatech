2ec51e7cbe8ce724401d03551834fb798c350101
Gayathri146 <103047218+Gayathriraju14@users.noreply.github.com>
2023-01-31 18:25:47 +0530

    Add files via upload

---
+++ Task 3b.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Vat
+    {
+        public void CalTax()
+        {
+            Console.WriteLine("Enter the amount in double precision:");
+            double amt = Convert.ToDouble(Console.ReadLine());
+            double vat = 20;
+            double tax = (amt * vat) / 100;
+            Console.WriteLine("VAT:{0} ", tax);
+        }
+
+    }
+    class Gst:Vat
+    {
+        public new void CalTax()
+        {
+            Console.WriteLine("Enter the amount in double precision:");
+            double amt = Convert.ToDouble(Console.ReadLine());
+            double gst = 12;
+            double tax = (amt * gst) / 100;
+            Console.WriteLine("Gst:{0}", tax);
+        }
+    }
+
+
+    internal class Task3a
+    {
+        public static void Main(string[] args)
+        {
+            Vat gst = new Vat();
+            gst.CalTax();
+            Vat vat = new Vat();
+            vat.CalTax();
+
+        }
+
+    }
+}

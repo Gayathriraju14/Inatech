db97ae34f1e0c0a3d9e2b0e926c788dbde90bd9b
Gayathri146 <103047218+Gayathriraju14@users.noreply.github.com>
2023-02-24 20:48:15 +0530

    Add files via upload

---
+++ Database approach/Koremvc/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koremvc.Models;
+
+public partial class Department
+{
+    public int DepartmentId { get; set; }
+
+    public string? DepartmentName { get; set; }
+
+    public decimal? Salary { get; set; }
+
+    public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
+}
---
+++ Database approach/Koremvc/Program.cs
@@ -0,0 +1,40 @@
+﻿using Koremvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Koremvc
+{
+    internal class Program
+    {
+        public static void InsertDepartment()
+        {
+            using (var dbs = new NewProcessContext())
+            {
+                Department department = new Department();
+                department.DepartmentName = "ECE";
+                dbs.Add(department);
+                // department.DepartmentId = 1;
+                //  dbs.Add(department); u should a not enter a the primary key in the code///
+                department = new Department();
+                department.DepartmentName = "CS";
+             //   department.DepartmentId = 2;
+                dbs.Add(department);
+                dbs.SaveChanges();
+            }
+            //public static void InsertEmployee()
+            //{
+            //    using (var dbs = new P)
+            //}
+        }
+        public static void Main(string[] args)
+        {
+            InsertDepartment();
+
+
+        }
+    }
+}
